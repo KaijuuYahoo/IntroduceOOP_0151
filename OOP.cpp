76a566b29b7c24c2a2295f33585c2116dcaefc98
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-04-29 18:32:28 +0700

    Menambahkan Prosedure Print Data pada Class Mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 class Mahasiswa{
     public :
-        int NIM;
+        int nim;
         string nama;
         float nilai;
+
+        void printData(){
+            cout << "NIM : " << nim << endl;
+            cout << "Nama : " << nama << endl;
+            cout << "Nilai : " << nilai << endl;
+
+        }
 };
 int main (){
 
